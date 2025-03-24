ee6cfa9c24ed2eef6171fc9390f82fc8c1dea474
almarzouk <jumaa.almarzouk@gmail.com>
2025-03-24 12:18:39 +0100

    github import

--- src/app/utils/auth.js
+++ src/app/utils/auth.js
@@ -1,13 +1,13 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
-import GothubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "./connect";
 import { getServerSession } from "next-auth";
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GothubProvider({
+    GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
