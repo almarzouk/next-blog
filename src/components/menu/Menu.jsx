97b0ab2c17e1dc6a25979bb77310ab8a274ba68b
almarzouk <jumaa.almarzouk@gmail.com>
2025-03-24 12:04:20 +0100

    test

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,6 @@
 import styles from "./menu.module.css";
-import { MenuPosts } from "../menuPosts/MenuPosts";
-import { MenuCategories } from "/menuCategories/menuCategories";
+import { MenuPosts } from "../menuPosts/MenuPosts.jsx";
+import { MenuCategories } from "../menuCategories/MenuCategories.jsx";
 function Menu() {
   return (
     <div className={styles.container}>
