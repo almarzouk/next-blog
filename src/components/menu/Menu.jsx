bd29dad3cf2382b4c0d7445a758e37c802682782
almarzouk <jumaa.almarzouk@gmail.com>
2025-03-24 12:01:30 +0100

    fix mmenu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,6 @@
 import styles from "./menu.module.css";
 import { MenuPosts } from "../menuPosts/MenuPosts";
-import { MenuCategories } from "../menuCategories/menuCategories";
+import { MenuCategories } from "/menuCategories/menuCategories";
 function Menu() {
   return (
     <div className={styles.container}>
